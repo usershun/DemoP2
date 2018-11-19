9efccf2f383d15c5df41704c0d2bac2f225af413
usershun <40844623+usershun@users.noreply.github.com>
2018-11-19 18:22:06 +0800

    Update PingResult2.java

--- app/src/main/java/qi/com/demop/PingResult2.java
+++ app/src/main/java/qi/com/demop/PingResult2.java
@@ -318,7 +318,7 @@ public class PingResult2 extends AppCompatActivity{
                                         if(code == 200)
                                         {
                                             Toast.makeText(PingResult2.this,"数据成功发送",Toast.LENGTH_LONG).show();
-                                            sleep(9000);
+                                            sleep(4000);
                                             finish();
 
                                         }
